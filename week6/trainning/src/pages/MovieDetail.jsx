5298ccef964b595ba8a015529a7b696dc4c5059d
JoEunHyang <146572390+JoEunHyang@users.noreply.github.com>
2023-11-16 23:26:55 +0900

    Update MovieDetail.jsx

--- week6/trainning/src/pages/MovieDetail.jsx
+++ week6/trainning/src/pages/MovieDetail.jsx
@@ -6,7 +6,7 @@ export default function MovieDetail() {
   const { title } = useParams();
 
   //네비게이트 훅을 통해 페이지 이동 시에 넘겨 받은 정보
-	//이미지, 별점 등의 모든 정보들
+  //이미지, 별점 등의 모든 정보들
   const { state } = useLocation();
   console.log(title);
   console.log(state);
