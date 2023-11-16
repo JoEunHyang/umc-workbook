2ed1626cbfb7290a22049b270e86ea1918a4d011
JoEunHyang <146572390+JoEunHyang@users.noreply.github.com>
2023-11-17 01:34:57 +0900

    Update Ad.jsx

--- week5/src/Components/Ad.jsx
+++ week5/src/Components/Ad.jsx
@@ -8,9 +8,9 @@ export default function Ad(props) {
         <div>
             {isAdIn && <Untitled/>}
             {isAdIn ? (
-                <button onClick={onClickAdOut}>광고안보기</button>
+                <button onClick={onClickAdOut}>광고 안 보기</button>
             ): (
-                <button onClick={onClickAdIn}>광고보기</button>
+                <button onClick={onClickAdIn}>광고 보기</button>
             )}
         </div>
     )
